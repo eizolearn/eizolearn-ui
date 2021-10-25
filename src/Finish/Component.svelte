0eaa99fc14367205d4659fa46064486def8d0a2d
Alexander Bzikadze <alexander.bzikadze@gmail.com>
2021-10-25 21:37:38 +0900

    Add finish page
    * Add finish page logic;
    * Change boolean properties and related css classes to "isSomething" pattern.

---
+++ src/Finish/Component.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte'
+    import CheckMark from './checkmark.svg'
+    import Refresh from './refresh.svg'
+
+    const dispatch = createEventDispatcher<{ refresh: void }>()
+</script>
+
+<div class="flex flex-col justify-center items-center mx-auto object-contain">
+    <CheckMark viewBox="0 0 20 20" class="w-36 h-36 fill-current" />
+    <h1>Learning Completed!</h1>
+    <div on:click="{() => dispatch('refresh')}">
+        <Refresh class="" />
+    </div>
+</div>
